45705a9a706cd57af7084726f88d251dfa9f2cb6
AnisaAshilah <98804531+AnisaAshilah@users.noreply.github.com>
2022-03-31 11:09:47 +0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,79 @@
+let planets = []
+let moon
+let numPlanets = 6
+let G = 120
+let destabilise = 0.15
+
+function setup() {
+  createCanvas(windowWidth,windowHeight)
+  moon = new Body(30,createVector(0,0),createVector(0,0))
+
+    // Planet
+  for (let i = 0; i < numPlanets; i++) {
+    let mass = random(5, 10)
+    let radius = random(moon.d, min(windowWidth/2,windowHeight/2))
+    let angle = random(0, TWO_PI)
+    let planetPos = createVector(radius * cos(angle), radius * sin(angle))
+
+    let planetVel = planetPos.copy()
+    if (random(1) < 0.1) planetVel.rotate(-HALF_PI)
+    else planetVel.rotate(HALF_PI)
+    planetVel.normalize()
+    planetVel.mult( sqrt((G * moon.mass)/(radius)) )
+    planetVel.mult( random( 1-destabilise, 1+destabilise) )
+
+    planets.push( new Body(mass, planetPos, planetVel) )
+  }
+}
+
+function draw() {
+  background(176,224,230)
+  translate(width/2, height/2)
+  for (let i = numPlanets-1; i >= 0; i--) {
+    moon.attract(planets[i])
+    planets[i].move()
+    planets[i].show()
+  }
+  moon.show()
+}
+
+
+function Body(_mass, _pos, _vel){
+  this.mass = _mass
+  this.pos = _pos
+  this.vel = _vel
+  this.d = this.mass*2
+  this.thetaInit = 0
+  this.path = []
+  this.pathLen = Infinity
+
+  this.show = function() {
+    stroke(0,50)
+    for (let i = 0; i < this.path.length-2; i++) {
+      line(this.path[i].x, this.path[i].y, this.path[i+1].x, this.path[i+1].y,)
+    }
+    fill(255); noStroke()
+    ellipse(this.pos.x, this.pos.y, this.d, this.d)
+  }
+
+
+  this.move = function() {
+    this.pos.x += this.vel.x
+    this.pos.y += this.vel.y
+    this.path.push(createVector(this.pos.x,this.pos.y))
+    if (this.path.length > 200) this.path.splice(0,1)
+  }
+
+  this.applyForce = function(f) {
+    this.vel.x += f.x / this.mass
+    this.vel.y += f.y / this.mass
+  }
+
+  this.attract = function(child) {
+    let r = dist(this.pos.x, this.pos.y, child.pos.x, child.pos.y)
+    let f = (this.pos.copy()).sub(child.pos)
+    f.setMag( (G * this.mass * child.mass)/(r * r) )
+    child.applyForce(f)
+  }
+
+}
